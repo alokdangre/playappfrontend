e06e3cf040576ebc18477ead1b36747fd873c48b
Studycode001 <148090007+Studycode001@users.noreply.github.com>
2024-12-23 21:35:53 +0530

    update routing and layout structure; add new pages and dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
 import { AuthProvider } from './context/AuthContext'
+import Header from './components/common/Header'
+import HomePage from './pages/HomePage'
+import VideoPage from './pages/VideoPage'
+import UserProfilePage from './pages/UserProfilePage'
+import UploadPage from './pages/UploadPage'
+import SearchResultsPage from './pages/SearchResultsPage'
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-        <div className="flex flex-col min-h-screen bg-gray-100">
-          <Navbar />
-          <main className="flex-grow container mx-auto px-4 py-8">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              {/* <Route path="/video/:videoId" element={<VideoPlayer />} /> */}
-              {/* <Route path="/channel/:username" element={<UserProfile />} />
-              <Route path="/upload" element={<PrivateRoute><VideoUpload /></PrivateRoute>} />
-              <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-              <Route path="/subscriptions" element={<PrivateRoute><Subscriptions /></PrivateRoute>} />
-              <Route path="/playlists" element={<PrivateRoute><Playlists /></PrivateRoute>} />
-              <Route path="/liked-videos" element={<PrivateRoute><LikedVideos /></PrivateRoute>} /> */}
-            </Routes>
-          </main>
-          <footer className="bg-gray-800 text-white py-4 text-center">
-            <p>&copy; 2023 VideoApp. All rights reserved.</p>
-          </footer>
+        <div className="min-h-screen bg-primary text-white">
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/video/:id" element={<VideoPage />} />
+            <Route path="/user/:username" element={<UserProfilePage />} />
+            <Route path="/upload" element={<UploadPage />} />
+            <Route path="/search" element={<SearchResultsPage />} />
+          </Routes>
         </div>
       </Router>
     </AuthProvider>
